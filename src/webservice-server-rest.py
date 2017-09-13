80fe38ed674e306de4d75557dba5bbb99edd4bcd
David Wyllie <david.wyllie@ndm.ox.ac.uk>
2017-09-13 20:52:09 +0100

    Change debug message

--- src/webservice-server-rest.py
+++ src/webservice-server-rest.py
@@ -492,7 +492,7 @@ def insert():
 		result = client.insert(guid, seq)
 		app.logger.critical("completed insert")
 	else:
-		abort(501, 'seq and guid are not present in the POSTed data {0}'.format(data.keys()))
+		abort(501, 'seq and guid are not present in the POSTed data {0}'.format(data_keys))
 	
 	#except Exception as e:
 	#	print("Exception raised", e)
