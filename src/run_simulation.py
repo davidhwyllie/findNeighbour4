2baf5c86d77b0724381b65ffb9ee666a7aec3ba0
David Wyllie <david.wyllie@ndm.ox.ac.uk>
2018-09-02 12:37:23 +0100

    Assess sensitivity of mixture detection

--- src/run_simulation.py
+++ src/run_simulation.py
@@ -10,6 +10,8 @@ import glob
 import numpy as np
 import json
 import argparse
+import random
+import math
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
@@ -60,12 +62,13 @@ python run_simulation.py  ../output/simulation_set_1""")
 
   
         # read data
+    
         with open(params_filename,'rt') as f:
             params = json.load(f)
         with open(observed_filename,'rt') as f:
             observed = pd.read_csv(f, sep='\t')
 
-        
+      
         ref_filename = os.path.join("..",'demos','simulation','config','ref.fasta')
         l = params['len_simulated_alignment']
         print("Making reference of length", l)
@@ -73,6 +76,7 @@ python run_simulation.py  ../output/simulation_set_1""")
         with open(ref_filename,'wt') as f:
             f.write(">reference\n{0}\n".format(miniseq))
   
+        print("Loading new data")
         fn3c.reset()        # restart server with no data and new reference sequence
         
         inserted = []
@@ -80,13 +84,16 @@ python run_simulation.py  ../output/simulation_set_1""")
             to_insert = observed.loc[ix,'seq']
             guid = observed.loc[ix,'node']
             fn3c.insert(guid, to_insert)
-            print("Loaded ",guid)
+            #print("Loaded ",guid)
             inserted.append(guid)
             if not len(inserted) == len(fn3c.guids()):
                 raise KeyError("Guids were not inserted correctly")
 
-        # examine each sample.  is it mixed?
-        for sample in inserted:
+        # examine all samples.  are they mixed?
+        print("Conducting per-sample analysis")
+        per_sample = []
+        for sample in  fn3c.guids():
+
             neighbours = fn3c.guid2neighbours(guid=sample, threshold = 20)
             nneighbours = len(neighbours)
             guids = [sample]
@@ -108,15 +115,76 @@ python run_simulation.py  ../output/simulation_set_1""")
                     outfile = os.path.join(inputdir,'msa_{0}.xlsx'.format(sample))
                     msa.to_excel(outfile)
     
-                
-            print(sample, len(neighbours), status, 'True mix = ',params['observed_sequence_mixed'])
-        exit()
-            # find its neighbours, possibly with downsampling
-        # make msa with sample + neighbours - this is what is proposed 
-        # is it mixed?
-        msa = fn3c.msa(inserted)
-        
-        print(ismixed)
-        print(params)
+            isCorrect = True
+            if status == 'multiple samples mixed':
+                isCorrect = False
+            if status == 'Not mixed' and (params['observed_sequence_mixed']==sample):
+                isCorrect = False
+            res = {'sim': observed_filename, 'guid':sample, 'nneighbours':len(neighbours),
+                   'mix_ascertainment':status, 'truly_mixed':params['observed_sequence_mixed']==sample,
+                   'isCorrect':isCorrect}
+            per_sample.append(res)
 
-    
+        outfile = os.path.join(inputdir,'persample.xlsx')
+        if len(per_sample)>0:
+            per_sample_results = pd.DataFrame.from_records(per_sample)
+            per_sample_results.to_excel(outfile)
+
+            # two outcomes
+            # proportion of samples correctly classified (all or none)
+            # proportion of samples which cannot be assessed
+
+            xtb = pd.crosstab(per_sample_results['isCorrect'], per_sample_results['mix_ascertainment'] )
+            xtb['simulation']=inputdir
+            xtb['nSamples']=len(per_sample)
+            outfile = os.path.join(inputdir,'persample_xtb.xlsx')
+            xtb.to_excel(outfile)
+        else:
+            print("Caution: no samples found ? path to simulated data is incorrect")
+        
+        # second analysis
+        # start with truly mixed sample
+        # find neighbours
+        # sample neighbours n=2 to n = ?10.  Up to 10 replicates per set of neighbours.
+        # are the samples detected as mixed?
+        # report proportion detected in the simulations
+        
+        # find the neighbours of the mixed sample, possibly with downsampling
+        for snv_cutoff in [5]:
+            neighbours = fn3c.guid2neighbours(guid=params['observed_sequence_mixed'], threshold = snv_cutoff)
+            nneighbours = len(neighbours)
+            neighbouring_guids = []
+            print("The mixed sample {0} has {1} neighbours at {2} snp; resampling".format(params['observed_sequence_mixed'], nneighbours, snv_cutoff))
+            
+            for item in neighbours:
+                neighbouring_guids.append(item[0])
+            if nneighbours >= 2:
+                # we sample the neighbours
+                max_neighbours = nneighbours
+                if max_neighbours > 20:
+                    max_neighbours = 20
+                sampled = []
+                for s in range(2,nneighbours):
+                    max_samples = math.factorial(s)     # max number of samples possible
+                    if max_samples > 5:
+                        max_samples = 5
+                    for i in range(max_samples):
+                        guid_sample = random.sample(neighbouring_guids, s)
+                        guid_sample.append(params['observed_sequence_mixed'])
+                        msa = fn3c.msa(guid_sample)
+                        #print(msa)
+                        p_value_cutoff = 1e-5/len(msa.index)
+                        ismixed = msa.query("p_value1 < @p_value_cutoff")
+                        status = 0
+                        if len(ismixed.index)==1:
+                            if params['observed_sequence_mixed'] in ismixed.index:
+                                status = 1
+                        sampling_result = {'dataset':inputdir, 'snv_cutoff':snv_cutoff, 'sample': i, 'sample_size':s, 'assessed_mixed': status, 'distance_between_mixed':params['distance_between_mixed']}
+                        sampled.append(sampling_result)
+                        #exit()
+            
+        outfile = os.path.join(inputdir,'resampling.xlsx')
+        if len(sampled)>0:
+            sampled_df = pd.DataFrame.from_records(sampled)
+            sampled_df.to_excel(outfile)
+            
