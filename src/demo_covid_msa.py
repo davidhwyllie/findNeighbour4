d4f9efd59e2212aecd2de0bd917b563132883069
David Wyllie <david.wyllie@phe.gov.uk>
2021-03-05 02:39:57 +0000

    Exemplar script producing clustered isolates & tree for RM review

--- src/demo_covid_msa.py
+++ src/demo_covid_msa.py
@@ -17,15 +17,21 @@ if __name__ == '__main__':
     from collections import Counter
     from fn4client import fn4Client
 
+
+    ############################################# INPUT #######################################################################
+    # LOAD THE LIST OF SAMPLES YOU WANT AN MSA FROM HERE
+    my_special_samples = set(['ALDP-12ABA31','ALDP-12AA63E'])           # pretend these are the samples of interest
+    snp_cutoff = 1                                                      # msa of samples within 1 snp of either of the above
+    output_file_stem = 'ALDP_31_3E'
+    ######################################### End of input ####################################################################
+
     # instantiate client
-    fn4c = fn4Client("http://findneighbours04.unix.phe.gov.uk:5023")      # expects operation on local host; pass baseurl if somewhere else.
+    fn4c = fn4Client("http://findneighbours04.unix.phe.gov.uk:5023")      # this is the experimental covid-19 server
 
     existing_guids = set(fn4c.guids())
     runtime = datetime.datetime.now().isoformat()
     print("Analysis run at {1}; there are {0} existing guids".format(len(existing_guids), runtime))
 
-    # LOAD THE LIST OF SAMPLES YOU WANT AN MSA FROM HERE
-    my_special_samples = set(['ALDP-12ABA31','ALDP-12AA63E'])           # pretend these are the samples of interest
 
     print("Analysing {0} samples, obtaining neighbourhood, and constructing msa.".format(len(my_special_samples)))
     missing_special_samples = my_special_samples - existing_guids
@@ -40,46 +46,64 @@ if __name__ == '__main__':
         for related_sample, distance in res['neighbours']:
                 for_msa.add(related_sample)
 
+    # optional - add outgroup/root
+    outgroup_name = '--Wuhan-Reference--'       # the reference sequence, which is in the server
+    for_msa.add(outgroup_name)
+
     # build an MSA
-    # various other kinds of output are possible including
-    #    json           # includes json-records but also positions of variation
-    #    json-records
+    # various kinds of output are possible including
+    #    json           # includes all info in json-records but also positions of variation, constant site numbers (for iqtree) etc
+    #    json-records   # a table
     #    html
     #    json-fasta
     #    fasta
         
-    # note that the for_msa call can return
+    # get a dictionary containing lots of details about the msa
     print("Building MSA with {0} sequences.".format(len(for_msa)))
-    msa_json= fn4c.msa(for_msa, output_format='json', what='N_or_M') 
-    print(msa_json)
-    msa_dict = json.loads(msa_json)
-
-    print(msa_dict.keys())
-
-    exit()
-
-    # to just get the MSA
-    msa_df = fn4c.msa(for_msa, output_format='json-records', what='N_or_M') 
-
-    # get all data
-    #res = json.loads(fn4c.msa(for_msa, output_format='json', what='N_or_M'))
+    msa_dict= fn4c.msa(for_msa, output_format='json', what='N_or_M') 
     
+    # returned object has keys as follows
+    # variant_positions - which positions the msa items correspond to
+    # valid/invalid_guids  - those that pass/fail %N cutoff
+    # expected_p1 - ignore, to do with %N in the sequences overall
+    # df_dict - convertable to a pandas dataframe
+    # what_tested in mixpore computation, N, M, or N_or_M
+    # outgroup - if specified
+    # fconst - constant sites outside the msa
+
+    print(msa_dict.keys())          # as above
+
+    print(msa_dict['fconst'])        # if needed can construct iqtree command with these
+    msa_df = pd.DataFrame.from_dict(msa_dict['df_dict'], orient='index')
+    print(msa_df)
+
+    bonferroni_corrected_p = 0.05 / len(msa_df.index)  
+    print("The following items failed a compositional (MIXPORE) test with more Ns/Ms than expected in the alignment; their positions in the ML tree are likely unstable")
+    print(msa_df.loc[msa_df['p_value3'] <= bonferroni_corrected_p])
+
     # export to excel
-    excel_outputfile = 'msa.xlsx' 
-    msa_df.to_excel(excel_outputfile)
+    excel_outputfile = '{0}.xlsx'.format(output_file_stem)
+    msa_df.to_excel(excel_outputfile)       #  study this carefully.  The p-values, if significant, are indicative of various types of mixture.
+                                            # see https://github.com/davidhwyllie/findNeighbour4/blob/master/doc/mixtureTesting.md
+                                            # I am not sure that the impact of mixtures on transmission has been thought about
+                                            # I think test 3 (p_value3) is probably the most appropriate
+                                            # and I think there is variation compatible with microvariation in at least 5% of sequences  - happy to discuss
+
+    # note: the column aligned_seq may contain M characters if there is an IUPAC code
+    # the column aligned_mseq will contain actual IUPAC code
+    # in general for iqTree etc you just want Ns - see below
 
     # export to fasta
     msa_creation_timestamp = datetime.datetime.now().isoformat()
     seqs = []
-    for ix in msa_df.index:
-        guid = msa_df.at[ix,'guid']
-        seq = msa_df.at[ix, 'aligned_seq'].replace('M','N')         # replace non  ACGT with N
+    for guid in msa_df.index:
+        seq = msa_df.at[guid, 'aligned_seq'].replace('M','N')         # replace non  ACGT mixed characters 'M' with N
         sr = SeqRecord(     Seq(seq), 
                             id= guid,
                             description="" )        # | Variant sites only shown within MSA created {0}".format(msa_creation_timestamp)
         seqs.append(sr)
 
-    fasta_outputfile =  'msa.fasta' 
+    fasta_outputfile =  '{0}.fasta'.format(output_file_stem) 
     with open(fasta_outputfile, 'w') as f:
         SeqIO.write(seqs, f, "fasta")
             
@@ -87,13 +111,26 @@ if __name__ == '__main__':
     if os.environ.get("IQTREE") is not None:
         iqtree_exe = os.environ.get("IQTREE")
         print("Path to iqtree executable found in environment variable, IQTREE")
-        cmd = '{0} -m GTR+G -mem 20G -o "{1}" -s {2} --redo'.format(iqtree_exe, outgroup_name, fasta_outputfile)
-        print(cmd)
+        run_ok = True
     else:
+        iqtree_exe = "<<INSERT PATH TO IQTREE EXECUTABLE HERE>>"
         print("No environment variable IQTREE found.  If using virtual environment, set it in the .env file.")
+        run_ok = False
 
+
+    cmd = '{0} -m GTR+G -mem 20G -o "{1}" -s {2} -fconst {3},{4},{5},{6} --redo'.format(
+        iqtree_exe, 
+        outgroup_name, 
+        fasta_outputfile, 
+        msa_dict['fconst']['A'], 
+        msa_dict['fconst']['C'], 
+        msa_dict['fconst']['G'], 
+        msa_dict['fconst']['T'])
 
     print("Complete.  Output is in {0} and {1}".format(fasta_outputfile, excel_outputfile))
     print("iqtree build command is \n{0}".format(cmd))
+    if run_ok:
+        os.system(cmd)
+        pass
 
  
