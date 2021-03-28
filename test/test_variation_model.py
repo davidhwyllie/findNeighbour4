d6c156dc5f948df0a8db9e96b702fabf226292ef
Brice Letcher <bletcher@ebi.ac.uk>
2021-03-28 18:13:12 +0100

    PCA covidtest code refactoring (3)
    * Use VariantMatrix and PCARunner classes. PCARunner runs off built
    VariantMatrix object.

--- test/test_variation_model.py
+++ test/test_variation_model.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from unittest.mock import MagicMock
 
-from findn.pca_covidtest_inc import SNPMatrix
+from findn.pca_covidtest_inc import VariantMatrix
 
 class DummyClass:
     pass
@@ -12,7 +12,7 @@ class TestGetPositionCounts(TestCase):
     def test_one_guid_correct_counts(self):
         PERSIST_object = DummyClass()
         PERSIST_object.refcompressedsequence_read = MagicMock(side_effect=[self.sample1])
-        analysed_samples, vmodel, mmodel = SNPMatrix.get_position_counts({"s1"}, 100, PERSIST_object)
+        analysed_samples, vmodel, mmodel = VariantMatrix.get_position_counts({"s1"}, 100, PERSIST_object)
         self.assertEqual(analysed_samples, {"s1"})
         expected_vmodel = {1: 1, 2: 1}
         expected_mmodel = {3: 1}
@@ -23,7 +23,7 @@ class TestGetPositionCounts(TestCase):
         PERSIST_object = DummyClass()
         PERSIST_object.refcompressedsequence_read = MagicMock(
                 side_effect=[self.sample1, self.sample2])
-        analysed_samples, vmodel, mmodel = SNPMatrix.get_position_counts(["s1", "s2"], 100, PERSIST_object)
+        analysed_samples, vmodel, mmodel = VariantMatrix.get_position_counts(["s1", "s2"], 100, PERSIST_object)
         self.assertEqual(analysed_samples, {"s1", "s2"})
         expected_vmodel = {1: 1, 2: 2}
         expected_mmodel = {3: 2, 4: 1}
@@ -34,13 +34,13 @@ class TestGetMissingness(TestCase):
     def test_get_missingness_cutoff(self):
         mmodel = {1: 2, 3: 12}
         selected_positions = [2,3,4,5]
-        cutoff = SNPMatrix.get_missingness_cutoff(selected_positions, mmodel)
+        cutoff = VariantMatrix.get_missingness_cutoff(selected_positions, mmodel)
         self.assertEqual(cutoff, 6)
 
     def test_remove_positions_with_high_missingness(self):
         mmodel = {1: 2, 3: 12}
         selected_positions = {1,2,3,4,5}
         cutoff = 6
-        num_removed = SNPMatrix.remove_high_missingness_positions(selected_positions, mmodel, cutoff)
+        num_removed = VariantMatrix.remove_high_missingness_positions(selected_positions, mmodel, cutoff)
         self.assertEqual(num_removed, 1)
         self.assertEqual(selected_positions, {1,2,4,5})
