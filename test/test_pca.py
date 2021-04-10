e43936a64b3f96bf2766db0510d8ec14dbef201e
David Wyllie <david.wyllie@phe.gov.uk>
2021-04-10 01:12:32 +0000

    Refactored pca, added unit tests

---
+++ test/test_pca.py
@@ -0,0 +1,107 @@
+""" integration test for findNeighbour4
+assumes a findNeighbour4 server is running, with the connection string stated in demos/AC587/config/config_nocl.json.
+
+An example command doing this would be 
+pipenv run python3 findNeighbour4_server.py demos/AC587/config/config_nocl.json
+
+The test loads the server with data from the AC587 test data
+and compares the SNP distances and links with those from a seqComparer
+instance.
+
+"""
+
+import os
+import glob
+import uuid
+import time
+import gzip
+import unittest
+import pickle
+import pandas as pd
+
+from findn import DEFAULT_CONFIG_FILE
+
+from pca.pca import TestPersistence, MNStats, VariationModel, VariantMatrix, PCARunner 
+from findn.common_utils import ConfigManager
+
+class Test_TestPersistence_1(unittest.TestCase):
+    """ tests the TestPersistence class"""
+    def runTest(self):
+        tp = TestPersistence(connstring='thing',number_samples=251)
+        tp.load_data(
+            sample_ids_file = "/data/software/fn4dev/testdata/pca/seqs_5000test_ids.pickle",
+            sequences_file = "/data/software/fn4dev/testdata/pca/seqs_5000test.pickle"
+        )
+        self.assertEqual(len(tp.seqs.keys()), 5000)
+        self.assertEqual(len(tp.sample_ids), 5000)
+        self.assertEqual(len(tp.refcompressedsequence_guids()), 5000)
+        res = tp.refcompressedsequence_read('NOT THERE')
+        self.assertEqual(res, None)
+
+        guids = list(tp.sample_ids)
+        res = tp.refcompressedsequence_read(guids[0])
+        self.assertIsInstance(res, dict)
+
+
+class Test_MNStats_1(unittest.TestCase):
+    """ tests the MNStats class"""
+    def runTest(self):
+        
+        rcs = {
+            'A': {28281, 5387, 23603, 23270}, 
+            'C': {6953, 15095, 16175, 24913, 28279}, 
+            'T': {5985, 3266, 27971, 12069, 14407, 4299, 15278, 28047, 240, 912, 28976, 14675, 23708, 23062, 28280, 3036, 28094}, 
+            'G': {24505, 23402, 28110}, 
+            'N': {21764, 21765, 21766, 21767, 21768, 21769, 11287, 11288, 11289, 11290, 11291, 11292, 11293, 11294, 11295, 21990, 21991, 21992, 28270}, 
+            'M': {}, 
+            'invalid': 0}
+
+        mns = MNStats(select_positions = list(range(29000)), analysed_reference_length = 29905)
+        res = mns.examine(rcs)
+        
+        self.assertIsInstance(res, dict)
+        self.assertEqual(res['N_total'],19)
+        self.assertEqual(res['M_total'],0)
+        
+       
+
+class Test_VariantMatrix_1(unittest.TestCase):
+    """ tests the VariantMatrix and PCARunner classes"""
+    def runTest(self):
+        
+        TPERSIST = TestPersistence(connstring='thing',number_samples=251)
+        TPERSIST.load_data(
+            sample_ids_file = "/data/software/fn4dev/testdata/pca/seqs_5000test_ids.pickle",
+            sequences_file = "/data/software/fn4dev/testdata/pca/seqs_5000test.pickle"
+        )
+        cfm = ConfigManager(DEFAULT_CONFIG_FILE)
+        CONFIG = cfm.read_config()
+        
+        v = VariantMatrix(CONFIG, TPERSIST, show_bar=False)
+
+        # test guids() method
+        self.assertEqual(set(v.guids()), set(TPERSIST.refcompressedsequence_guids()))
+
+        # test get_position_counts
+        guids,vmodel,mmodel = v.get_position_counts()
+        self.assertEqual(guids, set(TPERSIST.refcompressedsequence_guids()))
+        self.assertIsInstance(vmodel, dict)
+        self.assertIsInstance(mmodel, dict)
+       
+      
+        # test get_missingness_cutoff
+        m = v.get_missingness_cutoff(positions = vmodel.keys(), mmodel = mmodel)        # the missingness model
+        self.assertEqual(m, 27)     
+
+         # test build
+        v.build()
+        self.assertIsInstance(v.vm['variant_matrix'],pd.DataFrame)
+
+        # test run
+        pcr = PCARunner(v)
+        pcr.run(n_components= 10, pca_parameters = {})
+
+        # test cluster
+        v= pcr.cluster()
+
+        v.to_sqlite("unittest_tmp")
---
+++ utils/temporal_subsets.py
@@ -0,0 +1,96 @@
+""" generates lists of SARS-CoV-2 samples which occurred before a particular date
+Also generates a dictionary of reference compressed sequences
+And a subset of these
+
+Together, these can be passed to a ram_persistence object which 
+can be used instead of an fn3persistence object to test the performance of PCA, or for other 
+unit testing purposes.
+
+Also useful for investigating how PCA detected the ingress of new strains over time.
+Uses public cog metadata downloaded from COG-UK 7/4/2021, saved in
+testdata/pca/cog_metadata.csv.gz, and requires access to an fn3persistence object containing the same data.
+"""
+
+import os
+import json
+import pandas as pd
+import datetime
+import gzip
+import pickle
+import progressbar
+import random
+from findn.mongoStore import fn3persistence
+from findn.common_utils import ConfigManager
+
+# open connection to existing covid datastore
+config_file = os.path.join("..", "demos", "covid", "covid_config_v3.json")
+cfm = ConfigManager(config_file)
+CONFIG = cfm.read_config()
+PERSIST=fn3persistence(dbname = CONFIG['SERVERNAME'],           
+                        connString=CONFIG['FNPERSISTENCE_CONNSTRING'],
+                        debug=CONFIG['DEBUGMODE'])
+inputfile = "/data/software/fn4dev/testdata/pca/cog_metadata.csv.gz"
+outputdir = '/data/data/pca/subsets'        # or wherever
+
+# read metadata file into pandas
+with gzip.open(inputfile, 'rt') as f:
+    df = pd.read_csv(f)
+
+# we are using the middle part of the cog_id as the sample name as the sample_id; extract this.
+sample_ids = df['sequence_name'].to_list()
+df['sample_id'] = [x.split("/")[1] for x in sample_ids]
+
+# load a small subset of the reference compressed sequences, for testing purposes
+# load the reference compressed sequences
+storage_dict = {}
+sampled = random.sample(df['sample_id'].to_list(), 5000)
+bar = progressbar.ProgressBar(max_value = len(sampled))
+
+for i,sample_id in enumerate(sampled):
+    res = PERSIST.refcompressedsequence_read(sample_id)
+    bar.update(i)
+    storage_dict[sample_id]=res
+bar.finish()
+# write out the dictionary 
+outputfile = "/data/software/fn4dev/testdata/pca/seqs_5000test.pickle"
+with open(outputfile, 'wb') as f:
+    pickle.dump(storage_dict, f)
+outputfile = "/data/software/fn4dev/testdata/pca/seqs_5000test_ids.pickle"
+with open(outputfile, 'wb') as f:
+    pickle.dump(sampled, f)
+exit()
+# load the reference compressed sequences
+storage_dict = {}
+bar = progressbar.ProgressBar(max_value = len(df.index))
+for i,sample_id in enumerate(df['sample_id']):
+    res = PERSIST.refcompressedsequence_read(sample_id)
+    bar.update(i)
+    storage_dict[sample_id]=res
+bar.finish()
+
+# write out the dictionary 
+outputfile = os.path.join(outputdir, "seqs_20210401.pickle.gz")
+with gzip.open(outputfile, 'wb') as f:
+    pickle.dump(storage_dict, f)
+
+# construct counts between 1 June 2020 and end March 2021
+cnts = df.groupby(['sample_date']).size()
+cnts = cnts[cnts.index >= '2020-06-01']
+cnts = cnts[cnts.index < '2021-04-01']
+cnts = pd.DataFrame(cnts)
+cnts.columns = ['count']
+cnts['dow'] = [datetime.date.fromisoformat(item).weekday() for item in cnts.index]
+cnts['isodate'] = [datetime.date.fromisoformat(item) for item in cnts.index]
+
+# write samples to consider in the PCA into a series of json files in the output directory
+for cutoff_date in cnts.index:
+    dow = cnts.loc[cutoff_date, 'dow']
+    df_subset = df[df['sample_date']<cutoff_date]
+    sample_ids = df_subset['sample_id'].to_list()
+
+    outputfile = os.path.join(outputdir, "{0}-{1}.picklen.gz".format(dow, cutoff_date))
+    with gzip.open(outputfile, 'wb') as f:
+        pickle.dump(sample_ids, f)
+        print(outputfile)
+
+
