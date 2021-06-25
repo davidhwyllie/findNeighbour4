04ddf5fd9dc2b653e0885dc571d0ee13fc902e9c
David Wyllie <david.wyllie@phe.gov.uk>
2021-06-25 18:01:19 +0000

    alter unit test to write to unittest_tmp

--- test/test_tree.py
+++ test/test_tree.py
@@ -6,65 +6,74 @@ import pickle
 from tree.tree_utils import IQTree, ManipulateTree, DepictTree
 
 iqtree_requiring_test = unittest.skipIf(
-    os.getenv('IQTREE_DIR') is None, 'Test requires IQTREE v 2; no IQTREE environment found'
+    os.getenv("IQTREE_DIR") is None,
+    "Test requires IQTREE v 2; no IQTREE environment found",
 )
+
 
 @iqtree_requiring_test
 class Test_iqTree_1(unittest.TestCase):
-    """ test the iqTree module """
+    """test the iqTree module"""
 
     def runTest(self):
-        """ initialise """
+        """initialise"""
 
-        iqt = IQTree(genome_length = 30000)
+        iqt = IQTree(genome_length=30000)
 
         # read fasta file
-        inputfile = '/data/software/fn4dev/testdata/fasta/iqtree_test1.fasta'
-        with open(inputfile, 'rt') as f:
-            inputstring = f.read()       
-        cnt = Counter(['A', 'A', 'C', 'C', 'T', 'T', 'G'])
+        inputfile = "testdata/fasta/iqtree_test1.fasta"
+        with open(inputfile, "rt") as f:
+            inputstring = f.read()
+        cnt = Counter(["A", "A", "C", "C", "T", "T", "G"])
 
         with self.assertRaises(TypeError):
-            iqt.build(inputstring, 2, '/tmp/iqtree_test')           # cnt must be a dictionary
-        
-        # empty the target directory
-        targetdir= '/tmp/iqtree_test'
-        shutil.rmtree(targetdir)
-        iqt.build(inputstring, cnt, targetdir)           # cnt must be a dictionary
+            iqt.build(
+                inputstring, 2, "unittest_tmp/iqtree_test"
+            )  # cnt must be a dictionary
 
-        targetdir= '/tmp/iqtree_test'
+        # empty the target directory
+        targetdir = "/tmp/iqtree_test"
+        shutil.rmtree(targetdir)
+        iqt.build(inputstring, cnt, targetdir)  # cnt must be a dictionary
+
+        targetdir = "/tmp/iqtree_test"
         shutil.rmtree(targetdir)
 
         # check there is not data in the directory
-        testfile = os.path.join(targetdir, 'alignment.fa.iqtree')
+        testfile = os.path.join(targetdir, "alignment.fa.iqtree")
         self.assertFalse(os.path.exists(testfile))
 
-        iqt.build(inputstring, {'A': 200, 'C': 200, 'G': 200, 'T': 200}, targetdir)           # cnt must be a dictionary
+        iqt.build(
+            inputstring, {"A": 200, "C": 200, "G": 200, "T": 200}, targetdir
+        )  # cnt must be a dictionary
 
         # check that files are in the directory
-        testfile = os.path.join(targetdir, 'alignment.fa.iqtree')
+        testfile = os.path.join(targetdir, "alignment.fa.iqtree")
         self.assertTrue(os.path.exists(testfile))
 
+
 class Test_iqTree_2(unittest.TestCase):
-    """ test the iqTree module when there is no IQTREE_DIR environment variable """
+    """test the iqTree module when there is no IQTREE_DIR environment variable"""
 
     def runTest(self):
-        """ initialise """
+        """initialise"""
 
         # remove if present
         try:
-            del os.environ['IQTREE_DIR']
+            del os.environ["IQTREE_DIR"]
         except KeyError:
             pass
 
-        iqt = IQTree(genome_length = 30000)
+        iqt = IQTree(genome_length=30000)
 
         # read fasta file
-        inputfile = '/data/software/fn4dev/testdata/fasta/iqtree_test1.fasta'
-        with open(inputfile, 'rt') as f:
-            inputstring = f.read()       
+        inputfile = "testdata/fasta/iqtree_test1.fasta"
+        with open(inputfile, "rt") as f:
+            inputstring = f.read()
 
-        res = iqt.build(inputstring, 2, '/tmp/iqtree_test')           # cnt must be a dictionary
+        res = iqt.build(
+            inputstring, 2, "unittest_tmp/iqtree_test"
+        )  # cnt must be a dictionary
         self.assertIsNone(res)
 
 
@@ -78,22 +87,23 @@ class Test_ManipulateTree(unittest.TestCase):
 
         mt = ManipulateTree(treetxt)
 
-        #d1 = mt.rtd()
-        #print(mt.nodes())
+        # d1 = mt.rtd()
+        # print(mt.nodes())
 
         n1 = set([x for x in mt.nodes()])
         mt.reroot("1")
         n2 = set([x for x in mt.nodes()])
         self.assertEqual(n1 - n2, set([]))
 
-        #d2 = mt.rtd()
+        # d2 = mt.rtd()
 
         mt.remove_outgroup()
         n3 = set([x for x in mt.nodes()])
         self.assertEqual(n1 - n3, set(["1"]))
 
-        #d3 = mt.rtd()
-        #print(d1,d2,d3)
+        # d3 = mt.rtd()
+        # print(d1,d2,d3)
+
 
 class Test_DepictTree_1(unittest.TestCase):
     """tests the DepictTree class"""
@@ -104,9 +114,10 @@ class Test_DepictTree_1(unittest.TestCase):
             treetxt = f.read()
         with open("testdata/ete3/test18.pickle", "rb") as f:
             metadata = pickle.load(f)
-        
+
         mt = DepictTree(treetxt, metadata)
-        mt.render('analysis/test.svg')
+        mt.render("unittest_tmp/test.svg")
+
 
 class Test_DepictTree_2(unittest.TestCase):
     """tests the DepictTree class"""
@@ -115,7 +126,7 @@ class Test_DepictTree_2(unittest.TestCase):
 
         with open("testdata/ete3/test18.nwk", "rt") as f:
             treetxt = f.read()
-        
+
         mt = ManipulateTree(treetxt)
         mt.zero_small_branch_lengths()
         mt.generate_simplified_tree()
