0e0e0ad6eb5a2d894943a1751b5c519aa537bc8a
David Wyllie <david.wyllie@phe.gov.uk>
2021-07-24 22:53:47 +0000

    Update license to MIT

--- findn/NucleicAcid.py
+++ findn/NucleicAcid.py
@@ -6,14 +6,10 @@ Copyright (C) 2021 David Wyllie david.wyllie@phe.gov.uk
 repo: https://github.com/davidhwyllie/findNeighbour4
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.  See see <https://www.gnu.org/licenses/>.
+it under the terms of the MIT License as published
+by the Free Software Foundation.  See <https://opensource.org/licenses/MIT>, and the LICENSE file.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+ 
 """
 
 import hashlib
